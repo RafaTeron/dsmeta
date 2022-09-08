de387f5d23e91e80d2b833e0b224753b36371a78
Rafael Abreu <rafael_sabreu@hotmail.com>
2022-09-08 15:27:45 -0300

    Date select

--- backent/src/main/java/com/devsuperior/dsmeta/controller/controller.java
+++ backent/src/main/java/com/devsuperior/dsmeta/controller/controller.java
@@ -1,25 +1,32 @@
 package com.devsuperior.dsmeta.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
-import com.devsuperior.dsmeta.service.service;
+import com.devsuperior.dsmeta.service.SaleService;
+
 
 @RestController
 @RequestMapping(value = "/sales")
 public class controller {
-	
+
 	@Autowired
-	private service service;
+	private SaleService service;
+
 	
 	@GetMapping
-	public List<Sale> findSale() {
-		return service.findSale();
+	   public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue = "") String minDate, 
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate, 
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
 	}
-
+	
 }
+
--- backent/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backent/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -8,8 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
-
 @Entity
 @Table(name = "tb_sales")
 public class Sale {
